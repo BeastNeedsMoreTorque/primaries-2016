e1e1f282200162a2f107ca30d65dbc789730ce38
Adam Hooper <adam@adamhooper.com>
2015-12-28 14:42:23 -0500

    Show an SVG map.
    (It doesn't do anything yet, and it has no style.)

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,6 +1,8 @@
 //= require './vendor/d3.v3.js'
 //= require './vendor/topojson.v1.js'
 //= require './vendor/jquery-2.1.4.js'
+//
+//= require './states.js'
 
 function load_us_map() {
   // started with http://bl.ocks.org/mbostock/4090848/
@@ -13,19 +15,20 @@ function load_us_map() {
   var path = d3.geo.path()
     .projection(projection);
 
-  var svg = d3.select('main-svg')
-    .width(width)
-    .height(height);
+  var svg = d3.select('#main-map')
+    .attr('width', width)
+    .attr('height', height);
 
-  var g = svg.append('g');
+  var states = svg.append('g').attr('class', 'states');
 
   d3.json('/topojson/us.json', function(err, us) {
     if (err) throw err;
 
-    g.selectAll('path')
-      .data(topojson.feature(us, us.objects.states))
-      .attr('class', 'state')
-      .attr('d', path);
+    states.selectAll('path')
+      .data(topojson.feature(us, us.objects.states).features)
+      .enter().append('path')
+        .attr('class', 'state')
+        .attr('d', path);
   });
 }
 
---
+++ assets/javascripts/states.js
@@ -0,0 +1,2 @@
+var STATES = [
+];
