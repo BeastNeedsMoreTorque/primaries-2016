909842e991fe254d3ba97b8cd1a231141e8fbb29
Adam Hooper <adam@adamhooper.com>
2016-01-06 14:25:11 -0500

    /2016 and /2016/primaries. Nix /2016/primaries/:p

--- app/views/all_primaries_view.rb
+++ app/views/all_primaries_view.rb
@@ -1,17 +1,10 @@
 require_relative './base_view'
 
 class AllPrimariesView < BaseView
-  attr_reader(:party)
-
-  def initialize(party); @party = party; end
-
-  def output_path; "2016/primaries/#{party.id}.html"; end
-  def candidates; party.candidates; end
+  def output_path; "2016/primaries.html"; end
   def race_months; RaceDay.all.group_by{ |rd| rd.date.to_s[0...7] }.values; end
 
   def self.generate_all
-    Party.all.each do |party|
-      self.generate_for_view(AllPrimariesView.new(party))
-    end
+    self.generate_for_view(AllPrimariesView.new)
   end
 end
---
+++ app/views/home_view.rb
@@ -0,0 +1,9 @@
+require_relative './base_view'
+
+class HomeView < BaseView
+  def output_path; "2016.html"; end
+
+  def self.generate_all
+    self.generate_for_view(HomeView.new)
+  end
+end
