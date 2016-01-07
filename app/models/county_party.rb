0084542ed7fe3f0e1dffa448c66cabe483af8daa
Adam Hooper <adam@adamhooper.com>
2016-01-07 18:00:37 -0500

    Put more data in results.json

--- app/models/county_party.rb
+++ app/models/county_party.rb
@@ -1,4 +1,4 @@
-CountyParty = Struct.new(:fips_code, :party_id, :n_precincts_reporting, :n_precincts_total, :last_updated) do
+CountyParty = Struct.new(:fips_int, :party_id, :n_precincts_reporting, :n_precincts_total, :last_updated) do
   def self.all=(v); @all = v; end
   def self.all; @all; end
 end
--- app/views/primaries_results_view.rb
+++ app/views/primaries_results_view.rb
@@ -3,6 +3,8 @@ require_relative './base_view'
 require_relative '../models/candidate'
 require_relative '../models/candidate_county'
 require_relative '../models/candidate_state'
+require_relative '../models/county_party'
+require_relative '../models/race'
 
 class PrimariesResultsView < BaseView
   def output_path; "2016/primaries/results.json"; end
@@ -11,7 +13,9 @@ class PrimariesResultsView < BaseView
     JSON.dump(
       candidate_csv: candidate_csv,
       candidate_state_csv: candidate_state_csv,
-      candidate_county_csv: candidate_county_csv
+      candidate_county_csv: candidate_county_csv,
+      county_party_csv: county_party_csv,
+      race_csv: race_csv
     )
   end
 
@@ -30,6 +34,18 @@ class PrimariesResultsView < BaseView
   def candidate_county_csv
     header = "candidate_id,fips_int,n_votes\n"
     data = CandidateCounty.all.map{ |cc| "#{cc.candidate_id},#{cc.fips_int},#{cc.n_votes}" }.join("\n")
+    header + data
+  end
+
+  def county_party_csv
+    header = "fips_int,party_id,n_precincts_reporting,n_precincts_total,last_updated\n"
+    data = CountyParty.all.map{ |cp| "#{cp.fips_int},#{cp.party_id},#{cp.n_precincts_reporting},#{cp.n_precincts_total},#{cp.last_updated}" }.join("\n")
+    header + data
+  end
+
+  def race_csv
+    header = "party_id,state_code,n_precincts_reporting,n_precincts_total,last_updated\n"
+    data = Race.all.map{ |r| "#{r.party_id},#{r.state_code},#{r.n_precincts_reporting},#{r.n_precincts_total},#{r.last_updated}" }.join("\n")
     header + data
   end
 
