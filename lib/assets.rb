89e31e8a4189b8797665b1bca8243392dc729aac
Adam Hooper <adam@adamhooper.com>
2016-01-08 17:15:32 -0500

    Make it easier to add more assets

--- lib/assets.rb
+++ lib/assets.rb
@@ -6,6 +6,17 @@ require_relative './paths'
 module Assets
   extend Sprockets::DigestUtils
 
+  SprocketsAssets = %w(
+    javascripts/main.js
+    stylesheets/main.css
+    stylesheets/right-rail.css
+  )
+
+  StaticAssets = %w(
+    topojson
+    images
+  )
+
   def self.clear
     $logger.info("Clearing assets...")
 
@@ -17,7 +28,33 @@ module Assets
   def self.build
     $logger.info("Rebuilding assets...")
 
-    # Build JSON data files
+    self.build_dynamic_js
+    self.build_sprockets_assets
+    self.build_static_assets
+  end
+  # asset_path('main.css') -> '/2016/stylesheets/main-abcdef.css'
+  def self.asset_path(path)
+    path =~ /(.*)\.(css|js)$/
+
+    raise "invalid path #{path}" if !$0
+    dir = case $2
+      when 'css' then 'stylesheets'
+      when 'js' then 'javascripts'
+      else raise "invalid asset extension #{$2}"
+    end
+
+    "/2016/#{dir}/#{$1}-#{asset_digest_hex("#{dir}/#{path}")}.#{$2}"
+  end
+
+  def self.digest_file_at_path(path)
+    digest = digest_class.new
+    File.open(path, 'r') { |f| digest << f.read }
+    pack_hexdigest(digest.digest)
+  end
+
+  private
+
+  def self.build_dynamic_js
     require_relative '../app/models/state'
     $logger.debug('Writing javascripts/state.js')
     File.open("#{Paths.Assets}/javascripts/states.js", 'w') do |f|
@@ -32,13 +69,14 @@ module Assets
         });
         EOT
     end
+  end
 
-    # Create main.js and main.css
+  def self.build_sprockets_assets
     sprockets = Sprockets::Environment.new("#{Paths.Dist}/2016") do |env|
       env.logger = $logger
     end
     sprockets.append_path(Paths.Assets)
-    %w(javascripts/main.js stylesheets/main.css stylesheets/right-rail.css).each do |filename|
+    SprocketsAssets.each do |filename|
       asset = sprockets.find_asset(filename)
       dirname, basename = filename.split('/')
       FileUtils.mkpath("#{Paths.Dist}/#{dirname}")
@@ -46,33 +84,14 @@ module Assets
       asset.write_to("#{Paths.Dist}/2016/#{filename}")
       asset.write_to("#{Paths.Dist}/2016/#{asset.digest_path}")
     end
+  end
 
-    # Copy static assets
-    %w(topojson images).each do |filename|
+  def self.build_static_assets
+    StaticAssets.each do |filename|
       $logger.debug("Copying asset #{filename}")
       FileUtils.cp_r("#{Paths.Assets}/#{filename}", "#{Paths.Dist}/2016")
     end
   end
-
-  def self.main_css_path
-    @main_css_path ||= "/2016/stylesheets/main-#{asset_digest_hex('stylesheets/main.css')}.css"
-  end
-
-  def self.right_rail_css_path
-    @right_rail_css_path ||= "/2016/stylesheets/right-rail-#{asset_digest_hex('stylesheets/right-rail.css')}.css"
-  end
-
-  def self.main_js_path
-    @main_js_path ||= "/2016/javascripts/main-#{asset_digest_hex('javascripts/main.js')}.js"
-  end
-
-  def self.digest_file_at_path(path)
-    digest = digest_class.new
-    File.open(path, 'r') { |f| digest << f.read }
-    pack_hexdigest(digest.digest)
-  end
-
-  private
 
   def self.asset_digest_hex(filename)
     digest_file_at_path("#{Paths.Dist}/2016/#{filename}")
