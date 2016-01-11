766f1297ed4152e95c1d4f568474efbee95ee9aa
Adam Hooper <adam@adamhooper.com>
2016-01-11 18:12:16 -0500

    Make CandidateCounty simpler (and faster)

--- app/models/candidate_county.rb
+++ app/models/candidate_county.rb
@@ -1,5 +1,1 @@
-CandidateCounty = Struct.new(:database, :candidate_id, :county_id, :n_votes) do
-  def candidate; database.candidates.find(candidate_id); end
-  def county; database.counties.find(county_id); end
-  def fips_int; county.fips_int; end
-end
+CandidateCounty = Struct.new(:database, :candidate_id, :fips_int, :n_votes)
--- app/models/county.rb
+++ app/models/county.rb
@@ -1,3 +1,3 @@
 County = Struct.new(:database, :fips_int) do
-  def id; fips_int; end
+  alias_method(:id, :fips_int)
 end
