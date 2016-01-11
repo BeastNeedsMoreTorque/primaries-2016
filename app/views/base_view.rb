535f844bfa2487bf88df5479fe7d3aebfd387603
Adam Hooper <adam@adamhooper.com>
2016-01-11 16:48:09 -0500

    Use Hamlit, not Haml. Bit of a speedup.

--- app/views/base_view.rb
+++ app/views/base_view.rb
@@ -1,3 +1,5 @@
+require 'tilt'
+
 require_relative '../../lib/paths'
 
 class BaseView
@@ -16,9 +18,8 @@ class BaseView
   def render(options)
     if options[:partial]
       filename = File.expand_path("../../templates/_#{options[:partial]}.html.haml", __FILE__)
-      template = File.read(filename)
-      haml_engine = Haml::Engine.new(template, filename: filename)
-      haml_engine.render(self)
+      template = Tilt.new(filename)
+      template.render(self)
     end
   end
 
@@ -32,27 +33,26 @@ class BaseView
 
   protected
 
-  def self.template_name_to_haml_engine(template_name)
-    @template_name_to_haml_engine ||= {}
-    @template_name_to_haml_engine[template_name] ||= load_haml_engine(template_name)
+  def self.template_name_to_template(template_name)
+    @template_name_to_template ||= {}
+    @template_name_to_template[template_name] ||= load_template(template_name)
   end
 
-  def self.load_haml_engine(template_name)
+  def self.load_template(template_name)
     filename = File.expand_path("../../templates/#{template_name}.html.haml", __FILE__)
-    template = File.read(filename)
-    Haml::Engine.new(template, filename: filename)
+    Tilt.new(filename)
   end
 
   def self.generate_for_view(view)
     path = "#{Paths.Dist}/#{view.output_path}"
     $logger.debug("Generating #{path}")
-    output = render_view_haml(view)
+    output = render_view(view)
     self.write_contents(path, output)
   end
 
-  def self.render_view_haml(view)
-    haml_engine = template_name_to_haml_engine(view.template_name)
-    haml_engine.render(view)
+  def self.render_view(view)
+    template = template_name_to_template(view.template_name)
+    template.render(view)
   end
 
   def self.write_contents(path, contents)
