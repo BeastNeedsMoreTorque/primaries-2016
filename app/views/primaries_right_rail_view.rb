8879e2cae9158a1529f65bdd3c6695242c8195f5
nickyforster <nicky@nickyforster.nyc>
2016-01-13 16:59:47 -0500

    right rail data for preview case, styling for right rail, changed ids for states collection class

--- app/views/primaries_right_rail_view.rb
+++ app/views/primaries_right_rail_view.rb
@@ -1,10 +1,16 @@
 require_relative './base_view'
 require_relative '../../lib/primaries_embed_view'
+require_relative '../collections/parties'
+require_relative '../collections/states'
 
 class PrimariesRightRailView < BaseView
   include PrimariesEmbedView
 
   def output_path; '2016/primaries/right-rail.html'; end
+
+  def dem_candidates; database.candidates.select{ |cd| cd.party_id == 'GOP'}; end
+  def gop_candidates; database.candidates.select{ |cd| cd.party_id == 'GOP'}; end
+  def state_iowa; database.states.select{ |s| s.code == 'IA' }; end
 
   def self.generate_all(database)
     self.generate_for_view(PrimariesRightRailView.new(database))
