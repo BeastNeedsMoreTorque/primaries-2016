ed93e56cd68ad55bf568d8d08d07e87b472e48ea
nickyforster <nicky@nickyforster.nyc>
2016-01-13 12:12:30 -0500

    pollster table for all primaries, pollster column added to state race delegate/vote tables

--- app/views/all_primaries_view.rb
+++ app/views/all_primaries_view.rb
@@ -3,6 +3,9 @@ require_relative './base_view'
 class AllPrimariesView < BaseView
   def output_path; "2016/primaries.html"; end
 
+  def dem_candidates; database.candidates.select{ |cd| cd.party_id == 'Dem'}; end
+  def gop_candidates; database.candidates.select{ |cd| cd.party_id == 'GOP'}; end
+
   def self.generate_all(database)
     self.generate_for_view(AllPrimariesView.new(database))
   end
