3ef4bc471345b239c84c8292095ff6a5614475c6
Adam Hooper <adam@adamhooper.com>
2016-01-15 12:30:56 -0500

    Make race schedule table nicer

--- app/collections/races.rb
+++ app/collections/races.rb
@@ -9,4 +9,20 @@ Races = CollectionClass.new('races', 'race', Race) do
     @by_party_and_state ||= map{ |r| [ "#{r.party.id}-#{r.state.code}", r ] }.to_h
     @by_party_and_state["#{party.id}-#{state.code}"]
   end
+
+  def sorted_by_state_name_and_party_name
+    all.sort do |a,b|
+      c1 = a.state_name.<=>(b.state_name)
+      if c1 != 0
+        c1
+      else
+        c2 = a.party_name.<=>(b.party_name)
+        if c2 != 0
+          c2
+        else
+          a.race_day_id.<=>(b.race_day_id)
+        end
+      end
+    end
+  end
 end
