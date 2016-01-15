ab1d0901a63f59196ca903a6bbbe28e0b1e8cd2a
Adam Hooper <adam@adamhooper.com>
2016-01-15 16:04:43 -0500

    Don't crash on production

--- lib/env.rb
+++ lib/env.rb
@@ -5,15 +5,19 @@ require 'bundler/setup'
 
 # Before we load sprockets.rb, which is slow, monkey-patch its dependency
 # https://github.com/rails/sprockets/issues/17
-require 'sprockets/mime'
-module Sprockets::Mime
-  alias_method(:original_register_mime_type, :register_mime_type)
+begin
+  require 'sprockets/mime'
+  module Sprockets::Mime
+    alias_method(:original_register_mime_type, :register_mime_type)
 
-  def register_mime_type(mime_type, extensions: [], charset: nil)
-    if mime_type == 'application/javascript' || mime_type == 'text/css'
-      original_register_mime_type(mime_type, extensions: extensions, charset: charset)
+    def register_mime_type(mime_type, extensions: [], charset: nil)
+      if mime_type == 'application/javascript' || mime_type == 'text/css'
+        original_register_mime_type(mime_type, extensions: extensions, charset: charset)
+      end
     end
   end
+rescue LoadError
+  # Doesn't work on production. BOO. Oh well, no harm done.
 end
 
 Bundler.require(:default)
