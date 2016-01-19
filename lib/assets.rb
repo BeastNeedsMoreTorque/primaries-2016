1893aa00dcdb845ecb4525c555dbb30b55adc0e9
Shane Shifflett <shifflett.shane@gmail.com>
2016-01-19 17:22:04 -0500

    mobile ad styles

--- lib/assets.rb
+++ lib/assets.rb
@@ -17,6 +17,7 @@ module Assets
     javascripts/primary-right-rail.js
     stylesheets/main.css
     stylesheets/splash.css
+    stylesheets/mobile-ad.css
   )
 
   # Assets we serve with a sha1 digest.
