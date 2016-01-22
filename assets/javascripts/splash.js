eff5d9bbd487036428919f078674a3b135f98040
Shane Shifflett <shifflett.shane@gmail.com>
2016-01-22 15:24:57 -0500

    add county legend, countdown then up after polls

--- assets/javascripts/splash.js
+++ assets/javascripts/splash.js
@@ -1,12 +1,19 @@
 //= require './vendor/jquery-2.2.0.js'
 
 $(function() {
-  var iowa_starts = '2016-02-01 20:00:00 GMT'
+  var iowa_polls_close = new Date('2/2/2016 1:00:00 AM UTC');
   function timeTill(end) {
     var timeDifference = Date.parse(end) - Date.parse(new Date());
+
+    if(timeDifference > 0){
+      $(".countdown-container h3").text("POLLS CLOSE IN")
+    }else {
+      $(".countdown-container h3").text("POLLS CLOSED FOR")
+      timeDifference = Date.parse(new Date()) - Date.parse(end);
+    }
+
     return {
-      'total': Date.parse(end) - Date.parse(new Date()),
-      'days': Math.floor(timeDifference / (1000 * 60 * 60 * 24)),
+      'total': timeDifference,
       'hours': Math.floor((timeDifference / (1000 * 60 * 60)) % 24),
       'minutes': Math.floor((timeDifference / 1000 / 60) % 60),
       'seconds': Math.floor((timeDifference / 1000) % 60)
@@ -35,20 +42,22 @@ $(function() {
     var countiesReporting = 0;
     var totalPrecincts = 0;
     var precinctsReporting = 0;
+    var totalCounties = $(".counties").children().length;
     $(".counties").children().each(function(ele){
       fips = this.getAttribute("data-fips-int");
       obj = data[fips];
       totalPrecincts += obj["n_precincts_total"];
       precinctsReporting += obj["total_n_precincts_reporting"]
-      if(obj["total_n_precincts_reporting"] > 0){
+      if(obj["total_n_precincts_reporting"] == obj["n_precincts_total"]){
         countiesReporting++; 
         $(this).addClass("has-results");
       }
     });
+    $("#unreported-counties").text(totalCounties - countiesReporting)
     $("#counties-val").text(countiesReporting);
-    $("#precincts-val").text(((precinctsReporting/totalPrecincts)*100).toFixed(0) + "%")
+    //$("#precincts-val").text(((precinctsReporting/totalPrecincts)*100).toFixed(0) + "%")
   }
-
+  initClock("countdown", iowa_polls_close);
   new pym.Child();
   $.getJSON(window.location.toString().split('#')[0] + '.json', function(json) {
     fillSvg(json);
