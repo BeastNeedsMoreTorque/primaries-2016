7dd63fd88e56fa50975d3df6bb9b6099d0fa4493
Adam Hooper <adam@adamhooper.com>
2016-01-25 17:21:26 -0500

    Avoid double-notifying Airbrake

--- lib/with_airbrake.rb
+++ lib/with_airbrake.rb
@@ -1,24 +1,26 @@
 module WithAirbrake
+  # Runs the given block. If it crashes, notifies Airbrake and raises the error
   def self.run(&block)
-    if enabled?
-      begin
-        block.call
-      rescue => e
-        Airbrake.notify_sync(e)
-        raise e
-      end
-    else
-      puts "NO AIRBRAKE"
-      block.call
-    end
+    ensure_configured
+
+    # When we require airbrake (as in `ensure_configured`), it adds an at_exit
+    # handler. This is the worst thing in my tiny world right now, because I
+    # didn't ask for it and I can't turn it off, so AARGH. But whatever. Let's
+    # hope the caller doesn't expect this method to do the right thing.
+    #
+    # Here's what can happen when the block throws an unhandled exception:
+    #
+    # 1. Airbrake isn't configured. Calls the block.
+    # 2. Airbrake is configured, and there's no exception handler around `run`:
+    #    calls the block, and calls Airbrake.notify_sync with the exception.
+    # 3. Airbrake is configured, and there *is* an exception handler around
+    #    `run`: calls the block and does *not* call Airbrake.notify_sync with
+    #    the exception. Instead, calls Airbrake.notify_sync on some future
+    #    error that we didn't ask for.
+    block.call
   end
 
   private
-
-  def self.enabled?
-    ensure_configured
-    @enabled
-  end
 
   def self.ensure_configured
     if @enabled.nil?
