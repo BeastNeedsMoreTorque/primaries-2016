2a752e189c57ab12e0de56c2763e183b30c81df0
Adam Hooper <adam@adamhooper.com>
2016-01-26 11:10:59 -0500

    Simplify race timing model

--- app/models/race.rb
+++ app/models/race.rb
@@ -28,25 +28,36 @@ Race = Struct.new(:database, :ap_id, :race_day_id, :party_id, :state_code, :race
   def disabled?; !race_day || race_day.disabled?; end
   def enabled?; race_day && race_day.enabled?; end
   def n_delegates; state.n_delegates(party_id); end
-  def live?; date == database.today; end
 
-  # Whether a race is 'upcoming', 'live' or 'past'
+  # Determines whether a race is 'future', 'present' or 'past'.
   #
-  # Note: a race can be `live` even when it happened the day before, if poll
-  # results are trickling in slowly.
-  def status
+  # These times are relative to the _user_'s clock. In other words, if a race
+  # on Feb. 1 has polls that close at 2 a.m. on Feb. 2, then at 1 a.m. on Feb. 2
+  # the race will be 'future'; at 2 p.m. the race will be 'present', and when
+  # counting is over (later on Feb. 2), the race will be 'past'.
+  #
+  # JavaScript can live-update this information.
+  #
+  # Again, here are definitions of the return values:
+  #
+  # * 'future': voting hasn't finished
+  # * 'present': voting has finished; results are not all in
+  # * 'past': results are all in
+  def when_race_happens
     if !n_precincts_reporting.nil? && n_precincts_reporting > 0
       if n_precincts_reporting < n_precincts_total
-        'live'
+        'present'
       else
         'past'
       end
     else
-      # TODO handle the case in which the polls have closed but no precincts are
-      # reporting yet
-      'upcoming'
+      'future'
     end
   end
+
+  def present?; when_race_happens == 'present'; end
+  def past?; when_race_happens == 'past'; end
+  def future?; when_race_happens == 'future'; end
 
   def candidate_states
     @candidate_states ||= database.candidate_states.find_all_by_party_id_and_state_code(party_id, state_code).sort || []
