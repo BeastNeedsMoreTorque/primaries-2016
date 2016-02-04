6f4ba6c3315e7e11238ae0c4cc81bfcd25ecffe5
Shane Shifflett <shifflett.shane@gmail.com>
2016-02-04 17:21:03 -0500

    add pct of votes on mobile ad

--- app/models/race_day.rb
+++ app/models/race_day.rb
@@ -59,7 +59,7 @@ RaceDay = RubyImmutableStruct.new(:database, :id, :races_codified) do
   # "future" if no races are reporting
   def when_race_day_happens
     tenses = races.map(&:when_race_happens)
-    return "present"
+    return "past"
     if tenses.include? "present"
       "present"
     elsif tenses.first == "past"
