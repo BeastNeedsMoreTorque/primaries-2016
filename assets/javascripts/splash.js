f804b32e6ddba271a26b3bc2b6266d355e21a464
Shane Shifflett <shifflett.shane@gmail.com>
2016-02-04 13:43:36 -0500

    remove unused variables

--- assets/javascripts/splash.js
+++ assets/javascripts/splash.js
@@ -3,21 +3,14 @@
 //= require './position_svg_cities.js'
 $(function() {
   function fillSvg(data, precincts){
-    var countiesReporting = 0;
-    var totalPrecincts = 0;
-    var precinctsReporting = 0;
     var totalCounties = $(".counties").children().length;
     $("svg .counties").children().each(function(ele){
       fips = this.getAttribute("data-fips-int");
       obj = data[fips];
-      totalPrecincts += obj["n_precincts_total"];
-      precinctsReporting += obj["total_n_precincts_reporting"];
       if(obj["total_n_precincts_reporting"] == obj["n_precincts_total"] && obj["total_n_precincts_reporting"] != 0){
-        countiesReporting++; 
         $(this).addClass("has-results");
       }
     });
-    console.log(precincts)
     $("#unreported-counties").text(precincts['counties_outstanding']);
     $("#counties-val").html(precincts['counties_finished'] + " FINISHED <span id='precincts-val'>(" + precincts['reporting_precincts_pct_str'] + " of precincts)</span>");
   }
