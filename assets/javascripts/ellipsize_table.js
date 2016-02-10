29da4337800bae965cb3d6dc002854fdf91300f1
Adam Hooper <adam@adamhooper.com>
2016-02-10 17:45:17 -0500

    Don't change td.pollster width on click "SHOW MORE"

--- assets/javascripts/ellipsize_table.js
+++ assets/javascripts/ellipsize_table.js
@@ -1,20 +1,21 @@
 /**
- * Hides all but the first `n` <tr>s in a <table>'s <tbody>.
+ * Toggles a <table>'s "ellipsized" class, with buttons it adds after the
+ * <table>.
+ *
+ * Does nothing if there is no <tr class="ellipsized"> within the table.
  *
  * @param table Table to ellipsize
- * @param n_trs Number of <tr>s we'll display before truncating
- * @param hidden_class Clas name to add to <tr>s we're hiding
  * @param show_html HTML to display as a "show" button
  * @param hide_html HTML to display as a "hide" button
  */
-function ellipsize_table(table, n_trs, hidden_class, show_html, hide_html) {
+function ellipsize_table(table, show_html, hide_html) {
   var $table = $(table);
-  var $hidden_trs = $table.find('tbody tr:eq(' + (n_trs - 1) + ')').nextAll();
-  var hidden = true;
 
-  if ($hidden_trs.length == 0) return;
+  if ($table.find('tr.ellipsized').length == 0) return;
 
+  var ellipsized = $table.hasClass('ellipsized');
   var $container = $table.next();
+
   if (!$container.is('.ellipsize-table-prompt')) {
     $container = $('<div class="ellipsize-table-prompt"></div>');
     $table.after($container);
@@ -24,24 +25,24 @@ function ellipsize_table(table, n_trs, hidden_class, show_html, hide_html) {
 
   function toggle(ev) {
     ev.preventDefault();
-    if (hidden) show(); else hide();
+    if (ellipsized) show(); else hide();
   }
 
   function hide() {
-    hidden = true;
-    $hidden_trs.addClass(hidden_class);
+    ellipsized = true;
+    $table.addClass('ellipsized');
     $container.html(show_html);
   }
 
   function show() {
-    hidden = false;
-    $hidden_trs.removeClass(hidden_class);
+    ellipsized = false;
+    $table.removeClass('ellipsized');
     $container.html(hide_html);
   }
 
   hide();
 }
 
-$.fn.ellipsize_table = function(n_trs, hidden_class, show_html, hide_html) {
-  this.each(function() { ellipsize_table(this, n_trs, hidden_class, show_html, hide_html); });
+$.fn.ellipsize_table = function(show_html, hide_html) {
+  this.each(function() { ellipsize_table(this, show_html, hide_html); });
 };
