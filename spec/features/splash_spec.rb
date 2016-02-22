1f00f973b8d4e99a7eb4d7950deed83767525203
Adam Hooper <adam@adamhooper.com>
2016-02-22 16:15:35 -0500

    Test marking winner on splash

--- spec/features/splash_spec.rb
+++ spec/features/splash_spec.rb
@@ -8,12 +8,27 @@ describe 'The splash banner', type: :feature do
     source
   end
 
+  def ap_election_days_source_with_winner(candidate_id)
+    source = Database.default_ap_election_days_source
+    source.candidate_races.map! { |cr| cr.merge(ap_says_winner: (candidate_id == cr.candidate_id), huffpost_says_winner: false) }
+    source
+  end
+
   def quick_database(n_reporting, n_total)
     mock_database(
       '2016-02-01',
       '2016-02-01',
       focus_race_day_id: '2016-02-01',
       ap_election_days_source: ap_election_days_source_with_precincts_reporting(n_reporting, n_total)
+    )
+  end
+
+  def winner_database(candidate_id)
+    mock_database(
+      '2016-02-01',
+      '2016-02-01',
+      focus_race_day_id: '2016-02-01',
+      ap_election_days_source: ap_election_days_source_with_winner(candidate_id)
     )
   end
 
@@ -61,4 +76,17 @@ describe 'The splash banner', type: :feature do
     expect(page.first('.precincts', visible: true).text).to eq('>99% of precincts reporting')
   end
 
+  it 'should mark the winner in HTML' do
+    PrimariesSplashView.generate_all(winner_database('1746'))
+    visit('/2016/primaries/splash')
+    expect(page.first('tr.winner .candidate-name').text).to eq('Clinton')
+  end
+
+  it 'should mark the winner from JSON' do
+    PrimariesSplashView.generate_all(winner_database(nil))
+    PrimariesSplashResultsView.generate_all(winner_database('1746'))
+    visit('/2016/primaries/splash')
+    loop until page.evaluate_script('!jQuery.active')
+    expect(page.first('tr.winner .candidate-name').text).to eq('Clinton')
+  end
 end
