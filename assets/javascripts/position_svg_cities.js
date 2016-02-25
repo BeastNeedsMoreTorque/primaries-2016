e354496089ecf2cfc31afb988c76bfea10b06b76
Adam Hooper <adam@adamhooper.com>
2016-02-25 11:41:57 -0500

    Avoid more repaints in position_svg_cities()

--- assets/javascripts/position_svg_cities.js
+++ assets/javascripts/position_svg_cities.js
@@ -1,28 +1,7 @@
-function position_svg_cities() {
-  var svg = this;
-
-  var viewBoxStrings = svg.getAttribute('viewBox').split(' ');
-  // Assume origin x and y are `0`
-  var width = +viewBoxStrings[2];
-  var height = +viewBoxStrings[3];
-
-  var rects = []; // Array of { x, y, width, height } Objects we've placed
-
-  function rects_intersect(rect1, rect2) {
-    return rect1.x < rect2.x + rect2.width
-      && rect1.x + rect1.width > rect2.x
-      && rect1.y < rect2.y + rect2.height
-      && rect1.height + rect1.y > rect2.y;
-  }
-
-  function rect_fits(rect) {
-    if (rect.x < 0) return false;
-    if (rect.y < 0) return false;
-    if (rect.x + rect.width > width) return false;
-    if (rect.y + rect.height > height) return false;
-
-    return rects.every(function(rect2) { return !rects_intersect(rect, rect2); });
-  }
+function position_svg_cities(svgs) {
+  // We'll position all <text>s at once, to avoid repaints. That means going
+  // through all <svg>s before altering the DOM.
+  var tasks = []; // { g: <g>, text: <text>, x: Number, y: Number }
 
   function trial_rectangles(original_bbox) {
     var margin_x = 6; // px between dot and text
@@ -53,66 +32,96 @@ function position_svg_cities() {
     ];
   }
 
-  // Calculate the positions of all the <text> elements. Do this all at once,
-  // before any positioning -- that way, we won't repaint.
-  var texts = []; // Array of { text, rect, x, y }
-  Array.prototype.forEach.call(svg.querySelectorAll('text'), function(el) {
-    texts.push({
-      el: el,
-      bbox: el.getBBox(),
-      x: null, // output x
-      y: null  // output y
-    });
-  });
-  if (texts.length == 0) return;
+  function rects_intersect(rect1, rect2) {
+    return rect1.x < rect2.x + rect2.width
+      && rect1.x + rect1.width > rect2.x
+      && rect1.y < rect2.y + rect2.height
+      && rect1.height + rect1.y > rect2.y;
+  }
 
-  // Sort from north to south. Otherwise, in a situation like this:
-  //
-  // +---------------+
-  // |               |
-  // |          2    |
-  // |             1 |
-  // |               |
-  // |               |
-  // |               |
-  // +---------------+
-  //
-  // ... city #1's label would go above-left, and city #2's label would
-  // overlap no matter what.
-  //
-  // (See Kansas: Topeka and Overland Park.)
-  texts.sort(function(o1, o2) { return o1.bbox.y - o2.bbox.y; });
+  function build_tasks_for_svg(svg) {
+    var viewBoxStrings = svg.getAttribute('viewBox').split(' ');
+    // Assume origin x and y are `0`
+    var width = +viewBoxStrings[2];
+    var height = +viewBoxStrings[3];
 
-  texts.forEach(function(o) {
-    var potential_rects = trial_rectangles(o.bbox);
+    var ret = [];
+    var rects = []; // Array of { x, y, width, height } Objects we've placed
 
-    for (var i = 0; i < potential_rects.length; i++) {
-      var r = potential_rects[i];
-      var rect2 = { x: r[1], y: r[2], width: o.bbox.width, height: o.bbox.height };
-      if (rect_fits(rect2)) {
-        rects.push(rect2);
-        o.x = rect2.x;
-        o.y = rect2.y;
-        return;
-      }
+    function rect_fits(rect) {
+      if (rect.x < 0) return false;
+      if (rect.y < 0) return false;
+      if (rect.x + rect.width > width) return false;
+      if (rect.y + rect.height > height) return false;
+
+      return rects.every(function(rect2) { return !rects_intersect(rect, rect2); });
     }
 
-    console.warn('Could not position text', text);
+    var texts = []; // Array of { text, rect, x, y }
+    Array.prototype.forEach.call(svg.querySelectorAll('text'), function(el) {
+      texts.push({
+        el: el,
+        bbox: el.getBBox()
+      });
+    });
+    if (texts.length == 0) return ret;
+
+    var g = texts[0].el.parentNode;
+
+    // Sort from north to south. Otherwise, in a situation like this:
+    //
+    // +---------------+
+    // |               |
+    // |          2    |
+    // |             1 |
+    // |               |
+    // |               |
+    // |               |
+    // +---------------+
+    //
+    // ... city #1's label would go above-left, and city #2's label would
+    // overlap no matter what.
+    //
+    // (See Kansas: Topeka and Overland Park.)
+    texts.sort(function(o1, o2) { return o1.bbox.y - o2.bbox.y; });
+
+    texts.forEach(function(o) {
+      var potential_rects = trial_rectangles(o.bbox);
+
+      for (var i = 0; i < potential_rects.length; i++) {
+        var r = potential_rects[i];
+        var rect2 = { x: r[1], y: r[2], width: o.bbox.width, height: o.bbox.height };
+        if (rect_fits(rect2)) {
+          rects.push(rect2);
+          ret.push({ g: g, text: o.el, x: rect2.x, y: rect2.y });
+          return;
+        }
+      }
+
+      console.warn('Could not position text', text);
+    });
+
+    return ret;
+  }
+
+  Array.prototype.forEach.call(svgs, function(svg) {
+    build_tasks_for_svg(svg).forEach(function(task) {
+      tasks.push(task);
+    });
   });
 
-  // Set the position of every <text>
-  texts.forEach(function(o) {
-    o.el.setAttribute('x', o.x);
-    o.el.setAttribute('y', o.y);
-  });
+  tasks.forEach(function(task) {
+    // Position the <text> according to our calculations
+    task.text.setAttribute('x', task.x);
+    task.text.setAttribute('y', task.y);
 
-  // Turn each <text> into two: <text class="background"> and <text class="foreground">.
-  var cities = texts[0].el.parentNode;
-  texts.forEach(function(o) {
-    var text2 = o.el.cloneNode(true);
-    o.el.setAttribute('class', 'background');
+    // Copy the <text> into two: a <text class="background" and a <text class="foreground">
+    var text2 = task.text.cloneNode(true);
+
+    task.text.setAttribute('class', 'background');
     text2.setAttribute('class', 'foreground');
-    cities.appendChild(text2);
+
+    task.g.appendChild(text2);
   });
 }
 
@@ -132,4 +141,6 @@ function position_svg_cities() {
  * * Must be called on one or more <svg> elements
  * * The <svg> element must have a `viewBox` attribute
  */
-$.fn.position_svg_cities = function() { this.each(position_svg_cities); };
+$.fn.position_svg_cities = function() {
+  position_svg_cities(this);
+};
