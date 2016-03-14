21aa8b26f7e1241fef258a38c1b17f56a896dfe8
Adam Hooper <adam@adamhooper.com>
2016-03-14 17:10:06 -0400

    Add sitemap

--- lib/uploader.rb
+++ lib/uploader.rb
@@ -30,7 +30,7 @@ class Uploader
   # delete the old fingerprints. By uploading them first, we guarantee we don't
   # upload an HTML file that points to an asset before that asset is uploaded.
   def upload_assets
-    Dir["#{Paths.Dist}/**/*.*"].select{ |s| s !~ /\.(html|json)$/ }.sort.each do |filename|
+    Dir["#{Paths.Dist}/**/*.*"].select{ |s| s !~ /\.(html|json|txt)$/ }.sort.each do |filename|
       content_type = if filename =~ /\.css$/
         'text/css; charset=utf-8'
       elsif filename =~ /\.png$/
@@ -41,6 +41,8 @@ class Uploader
         'image/gif'
       elsif filename =~ /\.svg$/
         'image/svg+xml'
+      elsif filename =~ /\.txt$/
+        'text/plain'
       elsif filename =~ /\.js$/
         'application/javascript; charset=utf-8'
       else
