7a1d14246e486f660db0853aa83c154274dbb044
Adam Hooper <adam@adamhooper.com>
2016-03-17 18:05:21 -0400

    Make the logger briefer

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,4 +1,7 @@
 require 'logger'
 
 $logger = Logger.new(STDERR)
+$logger.formatter = proc do |severity, datetime, progname, msg|
+  "#{datetime.strftime('%FT%T.%3N')} #{severity[0]}: #{msg}\n"
+end
 $logger.level = Logger.const_get((ENV['LOG_LEVEL'] || 'INFO').upcase)
