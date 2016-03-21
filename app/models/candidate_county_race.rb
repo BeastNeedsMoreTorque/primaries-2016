21643baeea1c386dd9a77a9abd98972d524a9d9e
Shane Shifflett <shifflett.shane@gmail.com>
2016-03-21 19:31:11 -0400

    add some helper methods, drop trump data by county

--- app/models/candidate_county_race.rb
+++ app/models/candidate_county_race.rb
@@ -12,6 +12,13 @@ CandidateCountyRace = RubyImmutableStruct.new(:database, :candidate_id, :fips_in
 
   def candidate; database.candidates.find!(candidate_id); end
   def candidate_slug; candidate.slug; end
+  def get_all_sibling_candidate_county_races; database.candidate_county_races.select{|ccr| ccr.fips_int == fips_int and ccr.candidate.party_id == candidate.party_id}; end
+  def total_geo_votes_candidate_party; get_all_sibling_candidate_county_races.map{|ccr| ccr.n_votes}.inject(0){|sum,x| sum + x }; end
+  def candidate_percent_votes; ((n_votes.to_f/total_geo_votes_candidate_party.to_f) * 100.0).round(1); end
+  def get_percent_votes_by_candidate
+    get_all_sibling_candidate_county_races.sort_by{|ccr| ccr.candidate.name}.map{|ccr| {ccr.candidate.name => ccr.candidate_percent_votes}}
+  end
+
 
   def <=>(rhs)
     if (x = race_id <=> rhs.race_id) != 0
