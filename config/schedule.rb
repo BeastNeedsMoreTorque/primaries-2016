d728176eaf18eb90e9944f2aad3a4e5f745677b5
Adam Hooper <adam@adamhooper.com>
2016-05-12 10:55:47 -0400

    Tweak schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,7 +16,7 @@ module ServerSchedule
   # (If we didn't wait until *after* the task finishes, we could end up
   # bunching API requests together, which could exhaust our quota. Better too
   # few requests than too many, because they're more likely to succeed.)
-  #TickIntervalInS = 1*60 # every 1min
+  TickIntervalInS = 1*60 # every 1min
 
   # The list of ticks.
   #
@@ -33,42 +33,43 @@ module ServerSchedule
   # * nothing: does nothing.
   #
   # Our limit is 10 API requests per minute.
-  #Ticks = [
-  #  del_super,
-  #  pollster,
-  #  election_day('2016-05-03'),
-  #  election_day('2016-04-26'),
-  #  election_day('2016-04-19'),
-  #  election_day('2016-04-09'),
-  #  election_day('2016-04-05'),
-  #  election_day('2016-03-26'),
-  #  election_day('2016-03-22'),
-  #  election_day('2016-03-15'),
-  #  election_day('2016-03-12'),
-  #  election_day('2016-03-10'),
-  #  election_day('2016-03-08'),
-  #  election_day('2016-03-06'),
-  #  election_day('2016-03-05'),
-  #  election_day('2016-03-01'),
-  #  election_day('2016-02-27'),
-  #  election_day('2016-02-23'),
-  #  election_day('2016-02-20'),
-  #  election_day('2016-02-09'),
-  #  election_day('2016-02-01')
-  #]
+  Ticks = [
+    del_super,
+    pollster,
+    election_day('2016-05-10'),
+    election_day('2016-05-03'),
+    election_day('2016-04-26'),
+    election_day('2016-04-19'),
+    election_day('2016-04-09'),
+    election_day('2016-04-05'),
+    election_day('2016-03-26'),
+    election_day('2016-03-22'),
+    election_day('2016-03-15'),
+    election_day('2016-03-12'),
+    election_day('2016-03-10'),
+    election_day('2016-03-08'),
+    election_day('2016-03-06'),
+    election_day('2016-03-05'),
+    election_day('2016-03-01'),
+    election_day('2016-02-27'),
+    election_day('2016-02-23'),
+    election_day('2016-02-20'),
+    election_day('2016-02-09'),
+    election_day('2016-02-01')
+  ]
 
   # On election night, do something like this:
   #
-  TickIntervalInS = 10
-  
-  Ticks = [
-    del_super,
-    election_day('2016-05-10'),
-    election_day('2016-05-10'),
-    election_day('2016-05-10'),
-    election_day('2016-05-10'),
-    election_day('2016-05-10')
-  ]
+  #TickIntervalInS = 10
+  #
+  #Ticks = [
+  #  del_super,
+  #  election_day('2016-05-10'),
+  #  election_day('2016-05-10'),
+  #  election_day('2016-05-10'),
+  #  election_day('2016-05-10'),
+  #  election_day('2016-05-10')
+  #]
 
   #
   # ... that will update votes five times per minute (average once per 12s) and
